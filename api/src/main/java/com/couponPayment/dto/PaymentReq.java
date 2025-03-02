ac689d69e3926a04acb56e27d01d3c9330d4efca
younghun <dudgns2043@naver.com>
2025-03-03 01:19:33 +0900

    토스 API, 결제 API, DTO, 통테 추가 yml엔 toss API key가 존재해 따로 암호화 할 방법 서칭중

---
+++ api/src/main/java/com/couponPayment/dto/PaymentReq.java
@@ -0,0 +1,16 @@
+package com.couponPayment.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+public class PaymentReq {
+    private String merchantId;
+    private String merchantMemberId;
+    private String orderNum;
+    private int installment;
+    private int amount;
+}
