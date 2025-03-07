79bee5ad1e17ac238921615958385005318ef4a8
younghun.lim <younghun.lim@seeroo.co.kr>
2025-03-07 17:41:07 +0900

    1. dto 추가

---
+++ api/src/main/java/com/couponPayment/entity/dto/WalletReqDto.java
@@ -0,0 +1,21 @@
+package com.couponPayment.entity.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+public class WalletReqDto {
+    private String merchantId;
+    private String merchantMemberId;
+    private String orderId;
+    private String orderNum;
+    private String amount;
+}
