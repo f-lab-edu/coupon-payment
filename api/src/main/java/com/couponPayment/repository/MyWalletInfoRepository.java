710e1574a17cff6fbd92a9c18021db6308f003a5
Lim-Young-hun <37940034+Lim-Young-hun@users.noreply.github.com>
2025-03-16 20:06:15 +0900

    Revert "Entity+repository (#8)"
    This reverts commit 197c318edd457ac570d99d59d6bfb23243023403.

--- api/src/main/java/com/couponPayment/repository/MyWalletInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.couponPayment.repository;
-
-import com.couponPayment.entity.MyWalletInfoTb;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MyWalletInfoRepository extends JpaRepository<MyWalletInfoTb, Long> {
-}
--- api/src/main/java/com/couponPayment/repository/StoreInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.couponPayment.repository;
-
-import com.couponPayment.entity.StoreInfoTb;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreInfoRepository extends JpaRepository<StoreInfoTb, Long> {
-}
